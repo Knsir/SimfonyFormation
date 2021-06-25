166af200d3420ae29b1d44462ef41d18e1eddecb
Ubuntu 18.04.5 LTS <karim.nsir@outlook.com>
2021-06-25 16:02:25 +0200

    commit 2

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
